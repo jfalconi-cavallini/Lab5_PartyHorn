6d01f8065ee217e868b5aadd81b2cb71b6ce3744
Jose Falconi <42155184+jfalconi-cavallini@users.noreply.github.com>
2021-02-04 19:26:51 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,19 +7,28 @@ function horn_Player(){
   document.getElementById("horn-sound").play();
 }
 
-function change_Horn(){
+function change_horn_sound(){
     if(document.getElementById("radio-air-horn").checked){
         document.getElementById("horn-sound").src = "./assets/media/audio/air-horn.mp3";
-        document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
     }
     else if(document.getElementById("radio-car-horn").checked){
         document.getElementById("horn-sound").src = "./assets/media/audio/car-horn.mp3";
-        document.getElementById("sound-image").src = "./assets/media/images/car.svg";
     }
     else if(document.getElementById("radio-party-horn").checked){
         document.getElementById("horn-sound").src = "./assets/media/audio/party-horn.mp3";
+    }
+}
+function change_horn_image(){
+  if(document.getElementById("radio-air-horn").checked){
+        document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
+    }
+    else if(document.getElementById("radio-car-horn").checked){
+        document.getElementById("sound-image").src = "./assets/media/images/car.svg";
+    }
+    else if(document.getElementById("radio-party-horn").checked){
         document.getElementById("sound-image").src = "./assets/media/images/party-horn.svg";
     }
 }
 document.getElementById("honk-btn").addEventListener("click",horn_Player);
-document.getElementById("audio-selection").addEventListener("change",change_Horn);
+document.getElementById("audio-selection").addEventListener("change",change_horn_sound);
+document.getElementById("audio-selection").addEventListener("change",change_horn_image);
