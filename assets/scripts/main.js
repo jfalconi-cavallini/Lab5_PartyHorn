ddf136a81009cab8b456092483a53b5681ed420f
Jose Falconi <42155184+jfalconi-cavallini@users.noreply.github.com>
2021-02-04 19:00:53 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,9 +2,24 @@
 
 // TODO
 
-function honk_Player(){
+function horn_Player(){
   event.preventDefault();
   document.getElementById("horn-sound").play();
 }
 
-document.getElementById("honk-btn").addEventListener("click",honk_Player);
+function change_Horn(){
+    if(document.getElementById("radio-air-horn").checked){
+        document.getElementById("horn-sound").src = "./assets/media/audio/air-horn.mp3";
+        document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
+    }
+    else if(document.getElementById("radio-car-horn").checked){
+        document.getElementById("horn-sound").src = "./assets/media/audio/car-horn.mp3";
+        document.getElementById("sound-image").src = "./assets/media/images/car.svg";
+    }
+    else if(document.getElementById("radio-party-horn").checked){
+        document.getElementById("horn-sound").src = "./assets/media/audio/party-horn.mp3";
+        document.getElementById("sound-image").src = "./assets/media/images/party-horn.svg";
+    }
+}
+document.getElementById("honk-btn").addEventListener("click",horn_Player);
+document.getElementById("audio-selection").addEventListener("change",change_Horn);
