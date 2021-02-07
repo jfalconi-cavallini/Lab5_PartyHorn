fba5135fe1847831a8c45f833825344c1f439957
Jose Falconi <42155184+jfalconi-cavallini@users.noreply.github.com>
2021-02-06 16:44:54 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -29,6 +29,30 @@ function change_horn_image(){
         document.getElementById("sound-image").src = "./assets/media/images/party-horn.svg";
     }
 }
+function update_volume_image(){
+    let original_volume = document.getElementById("horn-sound").volume * 100;
+    if(original_volume == 0){
+        document.getElementById("volume-image").src = "./assets/media/icons/volume-level-0.svg";
+        document.getElementById("honk-btn").disabled = true;
+    }
+    else if(original_volume < 34){
+        document.getElementById("volume-image").src = "./assets/media/icons/volume-level-1.svg";
+        document.getElementById("honk-btn").disabled = false;
+    }
+    else if(original_volume < 67){
+        document.getElementById("volume-image").src = "./assets/media/icons/volume-level-2.svg";
+        document.getElementById("honk-btn").disabled = false;
+    }
+    else{
+        document.getElementById("volume-image").src = "./assets/media/icons/volume-level-3.svg";
+        document.getElementById("honk-btn").disabled = false;
+    }
+}
+function uupdate_volume_sound(){
+    document.getElementById("horn-sound").volume = document.getElementById("volume-number").value / 100;
+    document.getElementById("volume-slider").value = document.getElementById("volume-number").value;
+    update_volume_image();
+}
 document.getElementById("honk-btn").addEventListener("click",horn_Player);
 document.getElementById("audio-selection").addEventListener("change",change_horn_sound);
 document.getElementById("audio-selection").addEventListener("change",change_horn_image);
